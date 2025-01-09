01bb02365259b16d95fc1ddc34ad1ce966408c0e
TungNguyen12 <nvtung382@gmail.com>
2025-01-09 17:30:05 +0200

    remove print

--- src/db/database.py
+++ src/db/database.py
@@ -5,6 +5,5 @@ from pymongo import MongoClient
 def get_db():
        """Returns a MongoDB database client."""
        uri = os.getenv("MONGODB_URI")
-       print(f"MONGODB_URI: {uri}")
        client = MongoClient(uri)
        return client.get_database("qr_code_app")
